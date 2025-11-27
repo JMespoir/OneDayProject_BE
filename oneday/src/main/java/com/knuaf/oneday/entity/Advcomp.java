06798d7cf9d993ced522ea9761771c36e433c5a6
JMespoir <maro3534@knu.ac.kr>
2025-11-28 03:53:59 +0900

    feat: 졸업요건CheckList 완료

--- oneday/src/main/java/com/knuaf/oneday/entity/Advcomp.java
+++ oneday/src/main/java/com/knuaf/oneday/entity/Advcomp.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "Advcomp")
+@Table(name = "advcomp")
 public class Advcomp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,5 +17,5 @@ public class Advcomp {
     private Integer abeek_general;//abeek 전문교양
     private Integer abeek_total; //abeek 총 학점
     private Integer base_major; // 전공기반
-    private Integer engin_major; // 공학 전공
+    private Integer engin_major; // 공학전공
 }
